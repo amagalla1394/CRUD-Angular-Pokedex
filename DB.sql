a8f18bf7a816626833fc251c3a81108d8d9a024f
Angel Magallanes <38112423+amagalla1394@users.noreply.github.com>
2022-05-30 19:08:10 -0500

    Create DB.sql

---
+++ DB.sql
@@ -0,0 +1,12 @@
+create database Pokedex;
+
+create table dexEntry (
+	pID int identity(1,1) primary key,
+	pName nvarchar(255) not null,
+	pTypeOne nvarchar(255) not null,
+	pTypeTwo nvarchar(255),
+	pTypeThree nvarchar(255)
+	);
+
+insert into dexEntry (pName, pTypeOne, pTypeTwo)
+values ('Bulbasaur', 'Grass', 'Poison');
